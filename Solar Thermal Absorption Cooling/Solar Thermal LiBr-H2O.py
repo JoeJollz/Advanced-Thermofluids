VC thermocycle Genetic algo parameter scaling fix

Solar Thermal Absorption Cooling/Solar Thermal LiBr-H2O.py
@@ -1206,17 +1206,9 @@ while solution_fitness_solar<0:
             - Mass flow rate (kg/s)
 
         '''
-        P_low = (solution_solar[0]-0)/(10-0)*(4.8-0.68)+0.68
-        if P_low<0.68 or P_low>2: # pressure constraints
-            return -np.inf
-        P_high = (solution_solar[1]-0) /(10-0)*(9.5-4.9)+4.9
-        if P_high >9.5 or P_high<4.9: # pressure constraints
-            return -np.inf
-    
-        
-        mass_flow = (solution_solar[2]-0)/(10-0)*(0.1-0.001)+0.001
-        if mass_flow >1 or mass_flow<0.0001: # mass flow constraints
-            return -np.inf
+        P_low = solution_solar[0]
+        P_high = solution_solar[1]        
+        mass_flow = solution_solar[2]
 
         
         m1 = m4 = m3 = m2 = mass_flow # kg/s